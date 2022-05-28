e371cc2e403f1729ce2953bc082eacf35d584486
antcarlosrbj <antoniocarlosrbj@gmail.com>
2022-05-28 16:07:30 -0300

    heroku bug fix

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import cors from 'cors';
 import express from 'express';
+import connection from './controllers/db.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
-
-app.get("/", (req, res) => res.send("Hello!!!"))
-
-
+app.get("/", async (req, res) => {
+    const result = await connection.query('SELECT * FROM categories');
+    res.send(result.rows)
+})
 
 const port = process.env.PORT || 4000;
 app.listen(port);
