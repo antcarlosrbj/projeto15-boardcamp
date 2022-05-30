68c86112cb7b7e6c938c405e60c423047fd44313
antcarlosrbj <antoniocarlosrbj@gmail.com>
2022-05-30 01:09:17 -0300

    customers (GET) 2 add filter cpf

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -4,7 +4,21 @@ import joi from 'joi';
 export async function customersGET(req, res) {
     try {
 
+        /* SEARCH IN DATABASE */
+
         const customers = await connection.query('SELECT * FROM customers');
+
+        /* FILTER (QUERY STRING) */
+
+        if (req.query.cpf) {
+            res.send(
+                customers.rows.filter(customer => {
+                    return RegExp('^' + req.query.cpf).test(customer.cpf)
+                })
+            )
+            return;
+        }
+
         res.send(customers.rows)
 
     } catch (error) {
@@ -16,7 +30,7 @@ export async function customersGET(req, res) {
 export async function customersGETifID(req, res) {
     try {
 
-       
+        
 
     } catch (error) {
         console.log("customersGET" + error);
